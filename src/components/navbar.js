626e44624a9dedfbd0538df2284217876ad2c447
soufianetinoudi <soufiane.tinoudigmail.com>
2023-10-17 17:36:57 +0100

    Added new components & created Navbar with items

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import './Navbar.css'
+import './Navbar.css';
+import mainLogo from '../assets/placeholder-colored.png' //porting the logo from assets folder to be used here
+import contactBtnIcon from '../assets/letter-icon-png-10.jpg'
+import { Link } from 'react-scroll'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
-  )
+    <nav className="navbar">
+      <img src={mainLogo} alt="myLogo" className="logo" />
+      <div className="desktopMenu">
+        <Link className="desktopMenuListItem">Home</Link>
+        <Link className="desktopMenuListItem">About</Link>
+        <Link className="desktopMenuListItem">Portfolio</Link>
+        <Link className="desktopMenuListItem">Clients</Link>
+      </div>
+      <button className="desktopMenuBtn">
+        <img src={contactBtnIcon} alt="" className="desktopMenuImg" /> Contact
+        Me
+      </button>
+    </nav>
+  );
 }
 
 export default Navbar
