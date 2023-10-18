cd9e86480f3f09896139b2c21dfbb27e6b52adf8
soufianetinoudi <soufiane.tinoudigmail.com>
2023-10-18 13:25:54 +0100

    Importing Skills component to App.js file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import Navbar from './components/Navbar'
-import Intro from './components/Intro';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Intro from "./components/Intro";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Intro />
+      <Skills />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
