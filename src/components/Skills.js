25da285f2b3ab7113237103711d85c7501ff9e8c
soufianetinoudi <soufiane.tinoudigmail.com>
2023-10-18 14:14:01 +0100

    Added new assets to Skills & a new section of code

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Skills.css";
 import reactLogo from "../assets/react_logo.png";
-import designLogo from "../assets/responsive_web_design11.png";
+import designLogo from "../assets/responsive-icon-9.png";
 import gitHubLogo from "../assets/github-octocat-logo-png-transparent.png";
+import appTestIcon from "../assets/test_mobile_app.png"
+import backendTestIcon from "../assets/backend_test.png"
+import reportIcon from "../assets/call-report-icon-3.png"
 
 const Skills = () => {
   return (
@@ -39,6 +42,47 @@ const Skills = () => {
           <img src={gitHubLogo} alt="gitHubIcon" className="skillBarImg" />
           <div className="skillBarText">
             <h2>Learn GitHub utility</h2>
+            <p>
+              This is a Placeholder Text and it's going be changed later on down
+              the line once the project is near its end
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <span className="skillTitle">What I did</span>
+      <span className="skillDesc">
+        I worked as a Quality Assurance tester for two years, testing on a
+        variety of projects and support the development team by creating details
+        reports
+      </span>
+      <div className="skillBars">
+        <div className="skillBar">
+          <img src={appTestIcon} alt="reactIcon" className="skillBarImg" />
+          <div className="skillBarText">
+            <h2>Testing mobile and web applications</h2>
+            <p>
+              This is a Placeholder Text and it's going be changed later on down
+              the line once the project is near its end
+            </p>
+          </div>
+        </div>
+
+        <div className="skillBar">
+          <img src={backendTestIcon} alt="designIcon" className="skillBarImg" />
+          <div className="skillBarText">
+            <h2>Backend testing</h2>
+            <p>
+              This is a Placeholder Text and it's going be changed later on down
+              the line once the project is near its end
+            </p>
+          </div>
+        </div>
+
+        <div className="skillBar">
+          <img src={reportIcon} alt="gitHubIcon" className="skillBarImg" />
+          <div className="skillBarText">
+            <h2>Creating detailed reports for the team</h2>
             <p>
               This is a Placeholder Text and it's going be changed later on down
               the line once the project is near its end
