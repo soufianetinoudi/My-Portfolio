032b66c84174b4a3d62d881aeab7f64119fe7e61
soufianetinoudi <soufiane.tinoudigmail.com>
2023-10-19 19:43:48 +0100

    Added footer component and its css

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <footer className="myFooter">
+      This portfolio was created using React Js. Made by Soufiane Tinoudi
+    </footer>
+  );
+};
+
+export default Footer;
