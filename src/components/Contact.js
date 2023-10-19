61f7b296a9211d8a6b6c0f714096447b7cf98589
soufianetinoudi <soufiane.tinoudigmail.com>
2023-10-19 17:41:52 +0100

    Added Contact component replacing Portfolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import './Contact.css'
+import React from "react";
+import "./Contact.css";
+import offptLogo from "../assets/ofppt-logo-ACD755B3D2-seeklogo.com.png";
+import fstLogo from "../assets/fstsettat.couleur-min-206x206.png"
 
 const Contact = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section id="contactPage">
+      <div id="education">
+        <h1 className="contactTitle">My Education</h1>
+        <p className="educationDesc">
+          For my education I studied Programming in different areas, giving me
+          the opportunity to learn more and expand my skills and knowledge
+        </p>
+        <div className="educationImgs">
+          <img src={offptLogo} alt="offpt_logo" className="educationImg"></img>
+          <img src={fstLogo} alt="fst_settat_logo" className="educationImg"></img>
+        </div>
+      </div>
+      <div id="Contact"></div>
+    </section>
+  );
+};
 
-export default Contact
-
+export default Contact;
