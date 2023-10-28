4a910ce20bb9ce132ef06fd8f5ab0f24935c445c
soufianetinoudi <soufiane.tinoudigmail.com>
2023-10-28 17:19:12 +0100

    Added Sign in validation file & imported to Signup

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -1,27 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
 import { Link } from 'react-router-dom'
+import Validation from './LoginValidation'
 
 function Signup() {
+
+    const [values, setValues] = useState({
+        name: '',
+        email: '', 
+        password: ''
+    })
+
+    const [errors, setErrors] = useState({})
+
+    const handleInput =(event) => {
+        setValues(prev => ({...prev, [event.target.name]: [event.target.value]}))
+    }
+
+    const handleSubmit =(event) => {
+        event.preventDefault();
+        setErrors(Validation(values));
+    }
+
   return (
 <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
       <div className="bg-white p-3 rounded w-25">
         <h2>Sign-Up</h2>
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
+
           <div className="mb-3">
             <label htmlFor="name"><strong>Name</strong></label>
-            <input type="text" placeholder="Enter Name" className='form-control rounded-0'/>
+            <input type="text" placeholder="Enter Name" name="name" onChange={handleInput} className='form-control rounded-0'/>
+            {errors.name && <span span className='text-danger rounded-0'>{errors.name}</span>}
           </div>
+
           <div className="mb-3">
             <label htmlFor="email"><strong>Email</strong></label>
-            <input type="email" placeholder="Enter Email" className='form-control rounded-0'/>
+            <input type="email" placeholder="Enter Email" name="email" onChange={handleInput} className='form-control rounded-0'/>
+            {errors.email && <span className='text-danger rounded-0'>{errors.email}</span>}
           </div>
+
           <div className="mb-3">
             <label htmlFor="password"><strong>Password</strong></label>
-            <input type="password" placeholder="Enter Password" className='form-control rounded-0'/>
+            <input type="password" placeholder="Enter Password" name="password" onChange={handleInput} className='form-control rounded-0'/>
+            {errors.password && <span className='text-danger rounded-0'>{errors.password}</span>}
           </div>
-          <button className='btn btn-success w-100 rounded-0'><strong>Sign up</strong></button>
+
+          <button type="submit" className='btn btn-success w-100 rounded-0'><strong>Sign up</strong></button>
           <p>You agree to our terms and policies</p>
           <Link to="/Login" className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Log in</Link>
+          
         </form>
       </div>
     </div>
