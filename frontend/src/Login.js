2c8cd269da230d4cfa75563cbf00c26458fa8e94
soufianetinoudi <soufiane.tinoudigmail.com>
2023-10-28 16:54:16 +0100

    Added login validation file & imported it to Login

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Validation from "./LoginValidation";
 
 // I'm using Bootstrap instead of css file for login, hence why some className have css values, those are possible through Bootstrap
 function Login() {
+    const [values, setValues] = useState({
+        email: '', 
+        password: ''
+    })
+
+    const [errors, setErrors] = useState({})
+
+    const handleInput =(event) => {
+        setValues(prev => ({...prev, [event.target.name]: [event.target.value]}))
+    }
+
+    const handleSubmit =(event) => {
+        event.preventDefault();
+        setErrors(Validation(values));
+    }
+
   return (
     <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
       <div className="bg-white p-3 rounded w-25">
         <h2>Log-In</h2>
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
+
           <div className="mb-3">
             <label htmlFor="email"><strong>Email</strong></label>
-            <input type="email" placeholder="Enter Email" className='form-control rounded-0'/>
+            <input type="email" placeholder="Enter Email" name="email" onChange={handleInput} className='form-control rounded-0'/>
+            {errors.email && <span className='text-danger rounded-0'>{errors.email}</span>}
           </div>
+
           <div className="mb-3">
             <label htmlFor="password"><strong>Password</strong></label>
-            <input type="password" placeholder="Enter Password" className='form-control rounded-0'/>
+            <input type="password" placeholder="Enter Password" name="password"  onChange={handleInput} className='form-control rounded-0'/>
+            {errors.password && <span className='text-danger rounded-0'>{errors.password}</span>}
+
           </div>
-          <button className='btn btn-success w-100 rounded-0'><strong>Log In</strong></button>
+          <button type="submit" className='btn btn-success w-100 rounded-0'><strong>Log In</strong></button>
           <p>You agree to our terms and policies</p>
           <Link to="/Signup" className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Create Account</Link>
+
         </form>
       </div>
     </div>
