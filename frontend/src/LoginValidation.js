e9cc3d87f4e111bb375393a8cacdd40f17c7c87c
soufianetinoudi <soufiane.tinoudigmail.com>
2023-10-28 17:19:38 +0100

    Corrected an error inside login validation file

--- frontend/src/LoginValidation.js
+++ frontend/src/LoginValidation.js
@@ -3,8 +3,8 @@ function Validation(values){
     const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
     const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
 
-    if(values.null === "") {
-        error.null = "Name should not be empty"
+    if(values.email === "") {
+        error.email = "Email should not be empty"
     }
     else if(!email_pattern.test(values.email)) {
         error.email = "Email didn't match"
